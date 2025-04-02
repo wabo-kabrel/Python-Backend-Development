2bcf403a278b49351b9648cc56ab97f2780f1021
Kabrel <wabokabrel@gmail.com>
2025-04-02 23:02:33 +0100

    Updated arithmeticOperators.py

---
+++ Python Revision Course/Python Basics/arithmeticOperators.py
@@ -0,0 +1,20 @@
+# Some terms
+# Unary Operators: Operators that require one operand to perform a specific operation.
+# Binary Operators: Require two operands to perform an operation
+# Operands: Variables, values, or expressions that are used with the operator to perform
+#a specific operation.
+
+
+# Arithmetic Operators
+# They include:
+    # Addition: +
+    # Subtraction: -
+    # Multiplication: *
+    # Division: /
+    # Modulus: %
+    # Exponent: **
+    # Floor Division: //
+
+# Unlike modulus or modulo which return the remainder, floor division returns the
+# whole number part. E.g.
+print (9//2)        # Ouput: 4
