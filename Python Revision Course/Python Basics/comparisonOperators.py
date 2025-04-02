384a1011e1d9c8d81800043b6d6efd1a703629a7
Kabrel <wabokabrel@gmail.com>
2025-04-02 23:30:53 +0100

    Updated comparisonOperators.py

---
+++ Python Revision Course/Python Basics/comparisonOperators.py
@@ -0,0 +1,7 @@
+# Comparison Operators
+    # Less than: <
+    # Greater than: >
+    # Less than or equal to: <=
+    # Greater than or equal to: >=
+    # Is equal to: ==
+    # Is not equal to: !=
