e61672be62bcbb7a0ea86791a33c8ecae72fec15
Kabrel <wabokabrel@gmail.com>
2025-04-02 23:56:46 +0100

    Updated bitwiseOperators.py and comparisonOperators.py

--- Python Revision Course/Python Basics/comparisonOperators.py
+++ Python Revision Course/Python Basics/comparisonOperators.py
@@ -5,3 +5,11 @@
     # Greater than or equal to: >=
     # Is equal to: ==
     # Is not equal to: !=
+    
+# Assignment Operator: =
+
+# Logical Operators: 
+    # AND operator: and
+    # OR operator: or
+    # NOT operator: not
+
