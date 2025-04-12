83d1ae4f62ea710553158fdaa3a1ec293a4bb527
Kabrel <wabokabrel@gmail.com>
2025-04-12 22:44:28 +0100

    Started with APIs

---
+++ Python Rest API/basicConcepts.py
@@ -0,0 +1,8 @@
+# What is an API?
+'''API stands for Application Programming Interface. It is like a waiter in a restaurant. You
+(the client) ask for food (data), the waiter (API) takes your order to the kitchen (server), and brings you the food (response).'''
+# APIs allow two applications to talk to each other. E.g. Your phone app sends a request to a weather server.
+
+# What is a REST API?
+
+
