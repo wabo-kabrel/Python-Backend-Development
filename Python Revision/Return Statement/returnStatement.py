30e96143ed706cb74702575477bb64c1a01885bc
Kabrel <wabokabrel@gmail.com>
2025-07-14 17:29:09 +0100

    Updates

---
+++ Python Revision/Return Statement/returnStatement.py
@@ -0,0 +1,9 @@
+# The return statement is used to get the response from the task being executed in a function.
+
+def add(a, b):
+    return a + b
+
+print(add(2, 3))  # Output: 5
+
+# One thing you should note is that the return statement marks the end of a function.
+# Any line of code written after the return statement in the same function with the return statement won't be executed. 
