c0649173c44a9a8ab08df9d38ae0d8634d8b50c2
Kabrel <wabokabrel@gmail.com>
2025-07-16 02:55:33 +0100

    Updates

--- Introduction to Django/Short Django Notes/projectStructure.py
+++ Introduction to Django/Short Django Notes/projectStructure.py
@@ -2,12 +2,14 @@
 # Django Project Structure
 
 '''
-mysite/                       # Root directory of the Django project
-    manage.py                 # CLI tool for administrative tasks
-    mysite/                   # Project directory containing settings and configuration
-        __init__.py           # Marks the directory as a Python package
-        settings.py           # Project settings and configurations
-        urls.py               # URL declarations for the project
-        wsgi.py               # WSGI application for deployment
+        
+        mysite/        ←  Root directory of the Django project
+├── manage.py          ← CLI tool to run Django commands
+├── mysite/            ← Project directory containing settings and configuration
+│   ├── __init__.py    ← Marks the directory as a Python package
+│   ├── settings.py    ← Project settings and configurations
+│   ├── urls.py        ← Root URL declarations for the project
+│   ├── asgi.py        ← ASGI application for deployment
+│   └── wsgi.py        ← WSGI application for deployment
 
 '''
