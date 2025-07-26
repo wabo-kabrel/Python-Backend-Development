0a41d62f297774b376e49838424fbf8bc3eea6e6
Kabrel <wabokabrel@gmail.com>
2025-07-26 23:22:58 +0100

    updates

---
+++ Introduction to Django/Short Django Notes/sendingdatatotemplates.py
@@ -0,0 +1,11 @@
+# Sending/Passing Data to Template File
+
+#1. Why Pass Data to Template?
+# - Templates display content to the user (HTML pages).But HTML alone is static. 
+#   You pass data from views.py to make your pages dynamic, like:
+    # - Showing a user’s name
+    # - Listing blog posts
+    # - Displaying a success message after form submission
+
+#2. The Basic Flow
+# - View (Python code) ➡ passes data ➡ to Template (HTML with DTL)
