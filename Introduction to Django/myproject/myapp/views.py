cfbe5aaf1846efd7bf8054db0de483d02110b5e4
Kabrel <wabokabrel@gmail.com>
2025-07-27 01:20:29 +0100

    Updates

--- Introduction to Django/myproject/myapp/views.py
+++ Introduction to Django/myproject/myapp/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    context = {
+        'name': 'Lamar',
+        'age': 19,
+        'nationality': 'Cameroonian'}
+    return render(request, 'index.html', context)
