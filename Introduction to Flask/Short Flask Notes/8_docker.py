512425bc28571188752fa092ea6f55f2c67cbcc7
Kabrel <wabokabrel@gmail.com>
2025-08-06 03:31:16 +0100

    Updates

---
+++ Introduction to Flask/Short Flask Notes/8_docker.py
@@ -0,0 +1,5 @@
+# Deployment of Flask app using Docker
+# - Creating a Basic Flask app
+# - Defining Requirements
+# - Creating Docker File & Built Container
+# - Utilizing Docker Container to Run the App
