5e2ac7cd8bfc63dd9a0e56882fadc99ae8c19752
Wabo Kabrel <162037931+wabo-kabrel@users.noreply.github.com>
2025-08-10 21:27:46 +0100

    Update decorators.py

--- Python Revision/Functions and Modules/decorators.py
+++ Python Revision/Functions and Modules/decorators.py
@@ -1,1 +1,18 @@
-Updates
+#1. What are decorators?
+# - Decorators in Python are used to add extra/special features to an existing 
+#   function without changing the function's original code.
+# - A decorator is represent with @ 
+
+
+#2. Real life example:
+# - In everyday life you have a plain donut 🍩 (your function).
+# - You add chocolate icing 🍫 (your decorator) — now your donut has more flavor
+#   without changing the inside.
+
+
+#3. How they work
+# - In Python, a decorator which is a funcfion, takes another function as an input.
+# - It returns a new function that adds something extra before/after calling
+# - the original function
+
+# - Basic structure: 
