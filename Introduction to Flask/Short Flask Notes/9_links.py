f0def5a6b90a163d3c7e193aae95d0a171db9675
Kabrel <wabokabrel@gmail.com>
2025-08-17 07:01:33 +0100

    Updates

---
+++ Introduction to Flask/Short Flask Notes/9_links.py
@@ -0,0 +1,4 @@
+# Why Links Matter
+# When your Flask app grows, you'll have multiple routes (pages). You 
+# need a way to connect these routes together using links (like a navbar, 
+# buttons, or references inside text).
